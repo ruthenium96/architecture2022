b9226d0431119eccd22e2b5c49735d1914981edd
Aleksandr Pertovsky <pertovskiy.aleksandr@gmail.com>
2022-02-27 06:46:34 +0300

    update shell.sh

--- shell.sh
+++ shell.sh
@@ -6,7 +6,7 @@ fi
 
 
 if [ $1 == "build" ]; then 
-    bazel build --config=clang_config //shell:shell
+    bazel build --repo_env=CC=clang //shell:shell
     if [ $? -eq 1 ]; then
         exit 1
     fi
