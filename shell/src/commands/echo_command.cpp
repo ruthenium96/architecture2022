7a348b165ab322f1bc64ba680b798b4e2c5681f0
ruthenium96 <emkad@yandex.ru>
2022-04-08 14:59:00 +0700

    Fix echo segfault

--- shell/src/commands/echo_command.cpp
+++ shell/src/commands/echo_command.cpp
@@ -3,10 +3,9 @@
 
 std::optional<int> shell::EchoCommand::execute(const Arguments& args, State& state, IStreams& stream) {
     auto& out_stream = stream.get_out_stream();
-    size_t index = 0;
-    for (; index + 1 < args.size(); ++index) {
+    for (size_t index = 0; index < args.size(); ++index) {
         out_stream << args[index] << " ";
     }
-    out_stream << args[index] << std::endl;
+    out_stream << std::endl;
     return 0;
 }
